2116fe237cd5a18faf283505c59e174f175e4c56
vynas <hamdabousnina@gmail.com>
2025-05-20 14:48:53 -0400

    sidebar component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,41 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import logo from "./logo.svg";
+import { links, social } from "./data";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, openSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" className="logo" />
+        <button className="close-btn" onClick={openSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 export default Sidebar;
